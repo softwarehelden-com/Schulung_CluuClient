42afbb47cf81d9e577eb12ccf29787ff5cae3bbf
Jörg Metzler <joerg.metzler@softwarehelden.com>
2023-07-17 10:41:56 +0200

    MaxWebRequests

---
+++ Tag1/MaxWebRequest/EnumerableExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Concurrent;
+
+namespace MaxWebRequest;
+
+internal static class EnumerableExtensions
+{
+    public static async Task ForEachAsync<T>(this IEnumerable<T> source, int dop, Func<T, Task> body, CancellationToken cancellationToken)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (body == null)
+        {
+            throw new ArgumentNullException(nameof(body));
+        }
+
+        async Task run(IEnumerator<T> partition, CancellationToken ct)
+        {
+            using (partition)
+            {
+                while (partition.MoveNext())
+                {
+                    await body(partition.Current).ConfigureAwait(false);
+
+                    ct.ThrowIfCancellationRequested();
+                }
+            }
+        }
+
+        await Task.WhenAll(
+            from partition in Partitioner.Create(source).GetPartitions(dop)
+            select run(partition, cancellationToken)
+        ).ConfigureAwait(false);
+    }
+}
---
+++ Tag1/MaxWebRequest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+var sw = Stopwatch.StartNew();
+
+using var httpClient = new HttpClient();
+using var sem = new SemaphoreSlim(10, 10);
+using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+
+var tasks = new List<Task<HttpResponseMessage>>();
+
+for (int i = 0; i < 100; i++)
+{
+    tasks.Add(getAsync(httpClient, sem, cts.Token));
+}
+
+await Task.WhenAll(tasks);
+
+sw.Stop();
+
+Console.WriteLine(sw.ElapsedMilliseconds);
+
+async Task<HttpResponseMessage> getAsync(HttpClient httpClient, SemaphoreSlim sem, CancellationToken cancellationToken)
+{
+    await sem.WaitAsync(cancellationToken);
+
+    try
+    {
+        using var response = await httpClient.GetAsync("https://google.de", cancellationToken);
+
+        response.EnsureSuccessStatusCode();
+
+        return response;
+    }
+    finally
+    {
+        sem.Release();
+    }
+}
